2f9f559dd77dcb6773f658effc9482aace07f274
root@kolkhoz <root@kolkhoz>
2008-11-08 20:47:53 +0100

    added errorhandling

--- deploy.sh
+++ deploy.sh
@@ -16,22 +16,34 @@ echo "IP: $IP"
 echo "VEROOT: $VEROOT"
 echo "Is this fine?"
 
+read lala
+
 exit
 
+check_rc() {
+RC=$?
+if [ $RC -gt 0 ]; then
+        echo "Failed in $1, RC was $RC. Bailing out"
+        exit 1
+fi
+}
 
+ 
 vzctl create $VEID --ipadd $IP --hostname $HN --ostemplate debian-4.1-bootstrap
+check_rc "vzctl create"
 
 debootstrap --exclude=dhcp-client,dhcp3-client,dhcp3-common,dmidecode,gcc-4.2-base,nano,module-init-tools,tasksel,tasksel-data,libdb4.4,libsasl2-2,libgnutls26,libconsole,libgnutls13,libtasn1-3,liblzo2-2,libopencdk10,libgcrypt11 --include=vim,mailx,mtr-tiny,screen,strace,ltrace,telnet,dnsutils,file,less,iptraf,lsof,rsync,unattended-upgrades,etckeeper --arch amd64 lenny $VEROOT http://ftp.at.debian.org/debian
+check_rc "debootstrap"
 
 perl -pi -e 's\^(?!#)(.*/sbin/getty)\#$1\' $VEROOT/etc/inittab
-
+check_rc "inittab edit"
 
 cat << EOF >> $VEROOT/etc/apt/apt.conf
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
 APT::Periodic::AuotcleanInterval 60;
 EOF
-
+check_rc "Editing apt.conf"
 
 
 #etckeeper
