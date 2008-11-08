df05162eee49c7d4230f890fd4e5df03f3dbf462
root@kolkhoz <root@kolkhoz>
2008-11-08 20:35:21 +0100

    first rough draft of deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,38 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo $1
+if [ $# -ne  3 ]; then
+	echo "Usage: $0 <VEID> <Hostname> <IP-Address>"
+	exit 1
+fi
 
 VEID=$1
+HN=$2
+IP=$3
+VEROOT=/var/lib/vz/private/$VEID
 
-echo $VEID
+echo "VEID: $VEID"
+echo "Hostname: $HN"
+echo "IP: $IP"
+echo "VEROOT: $VEROOT"
+echo "Is this fine?"
 
 exit
 
-debootstrap --exclude=dhcp-client,dhcp3-client,dhcp3-common,dmidecode,gcc-4.2-base,nano,module-init-tools,tasksel,tasksel-data,libdb4.4,libsasl2-2,libgnutls26,libconsole,libgnutls13,libtasn1-3,liblzo2-2,libopencdk10,libgcrypt11 --include=vim,mailx,mtr-tiny,screen,strace,ltrace,telnet,dnsutils,file,less,iptraf,lsof,rsync --arch amd64 lenny /var/lib/vz/private/$VEID http://ftp.at.debian.org/debian
 
-perl -pi -e 's\^(?!#)(.*/sbin/getty)\#$1\' /var/lib/vz/private/$VEID/etc/inittab
+vzctl create $VEID --ipadd $IP --hostname $HN --ostemplate debian-4.1-bootstrap
+
+debootstrap --exclude=dhcp-client,dhcp3-client,dhcp3-common,dmidecode,gcc-4.2-base,nano,module-init-tools,tasksel,tasksel-data,libdb4.4,libsasl2-2,libgnutls26,libconsole,libgnutls13,libtasn1-3,liblzo2-2,libopencdk10,libgcrypt11 --include=vim,mailx,mtr-tiny,screen,strace,ltrace,telnet,dnsutils,file,less,iptraf,lsof,rsync,unattended-upgrades,etckeeper --arch amd64 lenny $VEROOT http://ftp.at.debian.org/debian
+
+perl -pi -e 's\^(?!#)(.*/sbin/getty)\#$1\' $VEROOT/etc/inittab
 
 
-#vzctl create 3000 --ipadd 86.59.21.20 --hostname trottelkunde.amd.co.at --ostemplate debian-4.1-bootstrap
+cat << EOF >> $VEROOT/etc/apt/apt.conf
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::AuotcleanInterval 60;
+EOF
 
-# unattended-upgrades
-#
-#APT::Periodic::Update-Package-Lists "1";
-#APT::Periodic::Unattended-Upgrade "1";
-#APT::Periodic::AuotcleanInterval 60;
+
 
 #etckeeper
 #etckeeper init
