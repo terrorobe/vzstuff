3de82a62ae0862ce176f62d12984dc794b9046ac
root@kolkhoz <root@kolkhoz>
2008-11-08 21:20:49 +0100

    tidied up safety question added etckeeper setup

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,13 @@ echo "VEID: $VEID"
 echo "Hostname: $HN"
 echo "IP: $IP"
 echo "VEROOT: $VEROOT"
-echo "Is this fine?"
+echo "Is this fine? (y/n)"
 
-read lala
-
-exit
+read safetypin
+if [ $safetypin!="y" ]; then
+	echo "Aboritng at your discretion"
+	exit 1
+fi
 
 check_rc() {
 RC=$?
@@ -28,11 +30,13 @@ if [ $RC -ne 0 ]; then
 fi
 }
 
+unset LANG
  
 vzctl create $VEID --ipadd $IP --hostname $HN --ostemplate debian-4.1-bootstrap
 check_rc "vzctl create"
 
-debootstrap --exclude=dhcp-client,dhcp3-client,dhcp3-common,dmidecode,gcc-4.2-base,nano,module-init-tools,tasksel,tasksel-data,libdb4.4,libsasl2-2,libgnutls26,libconsole,libgnutls13,libtasn1-3,liblzo2-2,libopencdk10,libgcrypt11 --include=vim,mailx,mtr-tiny,screen,strace,ltrace,telnet,dnsutils,file,less,iptraf,lsof,rsync,unattended-upgrades,etckeeper --arch amd64 lenny $VEROOT http://ftp.at.debian.org/debian
+echo "Bootstrapping silently"
+debootstrap --exclude=dhcp-client,dhcp3-client,dhcp3-common,dmidecode,gcc-4.2-base,nano,module-init-tools,tasksel,tasksel-data,libdb4.4,libsasl2-2,libgnutls26,libconsole,libgnutls13,libtasn1-3,liblzo2-2,libopencdk10,libgcrypt11 --include=vim,mailx,mtr-tiny,screen,strace,ltrace,telnet,dnsutils,file,less,iptraf,lsof,rsync,unattended-upgrades,etckeeper,locales --arch amd64 lenny $VEROOT http://ftp.at.debian.org/debian > /dev/null
 check_rc "debootstrap"
 
 perl -pi -e 's\^(?!#)(.*/sbin/getty)\#$1\' $VEROOT/etc/inittab
@@ -45,12 +49,22 @@ APT::Periodic::AuotcleanInterval 60;
 EOF
 check_rc "Editing apt.conf"
 
+cat << EOF >> $VEROOT/root/etckickoff.sh
+#!/bin/bash
+etckeeper init
+cd /etc
+git commit -a -m "initial commit"
+EOF
+check_rc "Writing etckeeper script"
 
-#etckeeper
-#etckeeper init
-#cd /etc/
-#git ci -m "initial commit"
-#git commit -m "initial commit"
-#git gc
-#
+echo "Setting up etckeeper"
+chroot /var/lib/vz/private/$VEID /bin/bash /root/etckickoff.sh > /dev/null
+check_rc "Executing etckeeper script"
+
+
+rm $VEROOT/INSTANCEd $VEROOT/root/etckickoff.sh
+check_rc "Deleting setup files"
+
+echo "We completed successfully."
+
 
